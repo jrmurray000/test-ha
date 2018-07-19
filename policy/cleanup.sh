868ae12cd2408752a7b7e06e7b816ccfa4c158d3
Yu Liao <yliao@google.com>
2018-07-19 10:41:39 -0700

    added script to cleanup the policies for demo

---
+++ policy/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [[ $(basename `pwd`) != "org-policy" ]]; then
+  echo "Must be run in org-policy dir"
+  echo "../cleanup.sh"
+  exit 1
+fi
+rm -rf `ls |grep -v README`
+git add .
+git commit -m "cleanup namespaces and policies."
