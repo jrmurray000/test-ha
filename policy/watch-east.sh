c5fa273d7615bc6047d8b012e7ee2df1a80eb3ad
Yu Liao <yliao@google.com>
2018-07-19 16:15:18 -0700

    use shell script in case 'watch' is not available.

--- policy/watch-east.sh
+++ policy/watch-east.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-watch kubectl get namespaces --context gke_gke-on-prem-next-demo-2_us-east4-a_east-coast
+CMD="kubectl get namespaces --context gke_gke-on-prem-next-demo-2_us-east4-a_east-coast"
+while :; do clear;  echo $CMD; echo; $CMD; sleep 2; done
