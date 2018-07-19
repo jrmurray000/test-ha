8247657878424fe7952d11fd1e4cf50d571af111
Yu Liao <yliao@google.com>
2018-07-19 10:22:43 -0700

    revised demo.sh for Next

--- policy/demo.sh
+++ policy/demo.sh
@@ -3,7 +3,7 @@
 ########################
 # include the magic
 ########################
-. ../demo-magic.sh
+. ../../demo-magic.sh
 
 ########################
 # Configure the options
@@ -29,93 +29,30 @@ clear
 # show directory structure
 pe "tree"
 
-# show synced namespaces 
+# show git repo
+pe "git status"
+
+# show synced namespaces
 pe "kubectl get namespaces"
 
+# add policies
+pe "cp -r ../org-policy-backup/* ."
+pe "tree"
+pe "git add ."
+pe "git commit -m \"Adding namespaces and policies.\""
+pe "git push"
+pe "git status"
+
+# show synced namespaces
+pe "watch kubectl get namespaces"
+
+
 # show rolebinding inheritence from shipping-app-backend to shipping dev
-pe "cat org-policy/online/shipping-app-backend/pod-creator-rolebinding.yaml"
 pe "tree"
 pe "kubectl get rolebinding -n shipping-dev"
-
-# test inherited shipping-dev rolebinding for bob - secrets fails, pods succeeds
-pe "kubectl get secrets -n shipping-dev --as bob@foo-corp.com"
-pe "kubectl get pods -n shipping-dev --as bob@foo-corp.com"
-
-# demonstrate hierarchical resource quota
-pe "tree"
-pe "cat org-policy/online/shipping-app-backend/quota.yaml"
-
-# create a pod that exceeds the hierarchical quota
-pe "cat <<EOF | kubectl create --as bob@foo-corp.com -f -
-apiVersion: v1
-kind: Pod
-metadata:
-  name: busybox-sleep
-  namespace: shipping-prod
-spec:
-  containers:
-  - name: busybox
-    image: busybox
-    args:
-    - sleep
-    - \"1000000\"
-    resources:
-      requests:
-        memory: \"64Mi\"
-        cpu: \"2\"
-EOF"
-
-# reduce the CPU to 20%
-pe "cat <<EOF | kubectl create --as bob@foo-corp.com -f -
-apiVersion: v1
-kind: Pod
-metadata:
-  name: busybox-sleep
-  namespace: shipping-prod
-spec:
-  containers:
-  - name: busybox
-    image: busybox
-    args:
-    - sleep
-    - \"1000000\"
-    resources:
-      requests:
-        memory: \"64Mi\"
-        cpu: \"200m\"
-EOF"
-
-# create a namespace for the front end developers
-
-pe "mkdir org-policy/online/shipping-app-frontend"
-
-pe  "cat <<EOF > org-policy/online/shipping-app-frontend/namespace.yaml
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: shipping-app-frontend
-EOF"
-pe "git add ."
-pe "git commit -m \"Adding frontend namespace.\""
-pe "git push"
-
-# show namespace has synced
-pe "watch kubectl get ns"
-
-tree
-#missing a pod creator rolebinding for Bob
-pe "kubectl get rolebindings -n shipping-app-frontend"
-
-# move the rolebinding higher and push the commit
-pe "mv org-policy/online/shipping-app-backend/pod-creator-rolebinding.yaml org-policy/online/"
-pe "git add ."
-pe "git commit -m \"Moving rolebinding for shipping app pod creators.\""
-pe "git push"
-
-# show new rolebinding exists
-pe "watch kubectl get rolebindings -n shipping-app-frontend"
+pe "kubectl get resourcequota -n shipping-dev -o yaml"
 
 #show changes to cluster #2
-pe "kubectl config use-context gke_nomos-198616_us-west1-a_nomos-cluster-2"
-pe "kubectl get ns"
+#pe "kubectl config use-context gke_nomos-198616_us-west1-a_nomos-cluster-2"
+#pe "kubectl get ns"
 cmd
