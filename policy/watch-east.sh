a84aead682a5f773f497c5e06641adccc15c896c
Aparna Sinha <apsinha@apsinha-macbookpro.roam.corp.google.com>
2018-07-19 18:08:40 -0700

    used 'watch' command

--- policy/watch-east.sh
+++ policy/watch-east.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-CMD="kubectl get namespaces --context gke_gke-on-prem-next-demo-2_us-east4-a_east-coast"
-while :; do clear;  echo $CMD; echo; $CMD; sleep 2; done
+watch kubectl get namespaces --context gke_gke-on-prem-next-demo-2_us-east4-a_east-coast
